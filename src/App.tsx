421882fc0e0ffcaf304f207273dea3f83bebc68b
achmadakif <83754562+AchmadAkif@users.noreply.github.com>
2026-02-10 10:50:53 +0700

    feat: implement color change functionality and update TypeScript types

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleColorChange = async (colorValue: string) => {
+    const [tab] = await chrome.tabs.query({ active: true });
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id! },
+      args: [colorValue],
+      func: (color) => {
+        document.body.style.backgroundColor = color;
+      },
+    });
+  };
 
   return (
     <>
@@ -18,9 +26,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <input
+          type="color"
+          onChange={(e) => handleColorChange(e.currentTarget.value)}
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +38,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
