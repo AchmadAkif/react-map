1fa410a884a63c4675f2e469350e7b96533dc43e
achmadakif <83754562+AchmadAkif@users.noreply.github.com>
2026-02-16 11:10:05 +0700

    feat: monkey-patch devtools hook  "onCommitFiberRoot" function

--- extension/backend/installHook.ts
+++ extension/backend/installHook.ts
@@ -1,12 +1,50 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-const hasReactDevtoolsInstalled =
-  (window as any).__REACT_DEVTOOLS_GLOBAL_HOOK__ && true;
-const __REACT_DEVTOOLS_GLOBAL_HOOK__ = (window as any)
-  .__REACT_DEVTOOLS_GLOBAL_HOOK__;
+const __isReactMapDebugMode = true;
 
-if (hasReactDevtoolsInstalled) {
-  console.log("**installHook** has React Devtools");
-  console.log(__REACT_DEVTOOLS_GLOBAL_HOOK__);
-} else {
-  console.log("Please install React Devtool");
+// eslint-disable-next-line no-prototype-builtins
+const hasReactDevtoolsInstalled = window.hasOwnProperty(
+  "__REACT_DEVTOOLS_GLOBAL_HOOK__",
+);
+
+if (!hasReactDevtoolsInstalled) {
+  console.warn(
+    "[React-Map]: React Map requires React Dev Tools to be installed.",
+  );
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const reactInstances = (window as any).__REACT_DEVTOOLS_GLOBAL_HOOK__.renderers;
+const instance = reactInstances.get(1);
+const instanceVersion = instance.version;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const devtoolsHook = (window as any).__REACT_DEVTOOLS_GLOBAL_HOOK__;
+
+let __ReactMapFiberDOM;
+
+// Begin monkey-patch
+(function installHook() {
+  if (!hasReactDevtoolsInstalled) {
+    console.error(
+      "[React-Map] Error: React Devtools needs to be installed first before using this extension.",
+    );
+    return;
+  }
+
+  if (instance && instanceVersion) {
+    if (__isReactMapDebugMode)
+      console.log("[React-Map] React version: ", instanceVersion);
+
+    const __original_onCommitFiberRootFn = devtoolsHook.onCommitFiberRoot;
+
+    devtoolsHook.onCommitFiberRoot = function onCommitFiberRoot(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ...args: any[]
+    ) {
+      __ReactMapFiberDOM = args[1];
+
+      if (__isReactMapDebugMode)
+        console.log("[React-Map] DOM : ", __ReactMapFiberDOM);
+
+      return __original_onCommitFiberRootFn(...args);
+    };
+  }
+})();
