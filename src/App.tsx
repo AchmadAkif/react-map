ade0ba18d42eb3656209bdc28a6131c3d8cfeac6
achmadakif <83754562+AchmadAkif@users.noreply.github.com>
2026-03-06 23:13:58 +0700

    refactor: improve message handling by defining onMessageListener and updating source check

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import type { Message } from "../extension/backend/types";
 import "./App.css";
 
 function App() {
@@ -23,12 +24,21 @@ function App() {
       payload: "init",
     });
 
-    createdPort.onMessage.addListener((message) => {
-      if (message.source === "react-map-background") {
+    const onMessageListener = (message: Message) => {
+      if (message.source === "react-map-backend") {
         console.log(message);
       }
-    });
+    };
+
+    createdPort.onMessage.addListener(onMessageListener);
     portRef.current = createdPort;
+
+    return () => {
+      createdPort.disconnect();
+      if (portRef.current === createdPort) {
+        portRef.current = null;
+      }
+    };
   }, []);
 
   return <button onClick={handleClick}>Say Hi!</button>;
