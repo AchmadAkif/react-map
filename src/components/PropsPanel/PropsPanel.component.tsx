c2189dbae3f388a9b39f1cbe5a52a582f84157dc
achmadakif <83754562+AchmadAkif@users.noreply.github.com>
2026-05-15 21:30:54 +0700

    refactor: remove theme-related components and simplify rendering logic

--- src/components/PropsPanel/PropsPanel.component.tsx
+++ src/components/PropsPanel/PropsPanel.component.tsx
@@ -1,12 +1,10 @@
 // import { renderValue } from "../../utils";
 
 import { getValueTypeClass, renderValue } from "@/utils";
-import { useTheme } from "../theme-provider";
 import type { PropsPanelProps } from "./PropsPanel.types";
 
 const PropsPanel = ({ hoveredNode }: PropsPanelProps) => {
   const props = hoveredNode?.props ? hoveredNode.props : null;
-  const { resolvedTheme } = useTheme();
   return (
     <div className="flex flex-col px-4">
       <p className="font-bold">Props</p>
@@ -15,9 +13,7 @@ const PropsPanel = ({ hoveredNode }: PropsPanelProps) => {
           {Object.entries(props).map(([key, value]) => (
             <div key={key} className="flex gap-2">
               <dt className="min-w-20">{key}</dt>
-              <dd className={getValueTypeClass(value)}>
-                {renderValue(value, resolvedTheme)}
-              </dd>
+              <dd className={getValueTypeClass(value)}>{renderValue(value)}</dd>
             </div>
           ))}
         </dl>
